79e770f5668b0f7e5e28e4f0b5010dd7a4f216ed
Mariam <mariam.dgebuadze@helmes.com>
2020-02-27 23:37:52 +0200

    Added TimetableController Updated DriverController

--- src/main/java/controller/DriverController.java
+++ src/main/java/controller/DriverController.java
@@ -20,12 +20,13 @@ public class DriverController {
 				String myDriverName = resultSetDrivers.getString("fullName");
 				int myDriverAge = resultSetDrivers.getInt("age");
 				float myDriverHeight = resultSetDrivers.getFloat("height");
+
 				System.out.println(myDriverId + " " + myDriverName + " " + myDriverAge + " " + myDriverHeight);
 			}
 			statement.close();
 		} catch (SQLException ex) {
 				System.out.println(ex);
-			}
+		}
 	}
 
 
@@ -36,6 +37,24 @@ public class DriverController {
 			String newDriver = "INSERT INTO drivers (name, address, phoneNr, age, height) VALUES (\""
 					+ name + "\"," + "\"" + address + "\"," + "\"" + phoneNr  + "\"," + age  + "," +  height + ")";
 			statement.executeUpdate(newDriver);
+
+			statement.close();
+		}catch (SQLException e) {
+			System.out.println(e);
+		}
+	}
+
+	public void findDriverByBusNumber(int busNumber) {
+		Statement statement = databaseHandler.createStatement();
+		try {
+			String findDriver = "\tSELECT fullName FROM drivers\n" +
+					"\tINNER JOIN buses on buses.driverId = drivers.id\n" +
+					"\tWHERE buses.busNumber = " + busNumber;
+			ResultSet driverName = statement.executeQuery(findDriver);
+			if (driverName != null){
+				System.out.println("Driver " + driverName + " rides bus number " + busNumber);
+			} else
+				System.out.println("Bus or driver does not exist");
 
 			statement.close();
 			databaseHandler.closeConnection();
--- src/main/java/controller/TimetableController.java
+++ src/main/java/controller/TimetableController.java
@@ -5,7 +5,6 @@ import db.DatabaseHandler;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.sql.Time;
 
 public class TimetableController {
     DatabaseHandler databaseHandler = new DatabaseHandler();
@@ -18,11 +17,13 @@ public class TimetableController {
 
             while (results.next()) {
                 int myTimetableId = results.getInt("id");
+                int myTimetableBusId = results.getInt("busId");
                 int myTimetableLocationId = results.getInt("locationId");
                 String myTimetableWeekDay = results.getString("weekday");
                 String myTimetableArrivalTime = results.getString("arrivalTime");
 
-                System.out.println(myTimetableId+" "+myTimetableLocationId+" "+myTimetableWeekDay+" "+myTimetableArrivalTime);
+                System.out.println(myTimetableId+" "+myTimetableBusId+" "+myTimetableLocationId+" "+myTimetableWeekDay+" "+myTimetableArrivalTime);
+
             }
             statement.close();
         } catch (SQLException ex) {
@@ -31,13 +32,35 @@ public class TimetableController {
 
     }
 
-    public void createTimetable (int locationId, String weekDay, String arrivalTime) {
+
+    public void createTimetable (int busId, int locationId, String weekDay, String arrivalTime) {
         Statement statement = databaseHandler.createStatement();
 
         try {
             String newTimetable = "INSERT INTO timetable (locationId, weekDay, arrivalTime) VALUES ("
-                    + locationId + "," + "\"" + weekDay + "\"," + "\"" + arrivalTime + "\")";
+                    + busId + "," + locationId + "," + "\"" + weekDay + "\"," + "\"" + arrivalTime + "\")";
+
             statement.executeUpdate(newTimetable);
+
+            statement.close();
+            databaseHandler.closeConnection();
+
+        }catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+
+    public void findTimetableForBusNumber(int busNumber) {
+        Statement statement = databaseHandler.createStatement();
+        try {
+            String findTimetable = "\tSELECT * FROM timetable\n" +
+                    "\tINNER JOIN buses on buses.driverId = drivers.id\n" +
+                    "\tWHERE buses.busNumber = " + busNumber;
+            ResultSet busTimetable = statement.executeQuery(findTimetable);
+            if (busTimetable != null){
+                System.out.println("Here is timetable for bus number " + busNumber);
+            } else
+                System.out.println("Bus or timetable does not exist");
 
             statement.close();
             databaseHandler.closeConnection();
