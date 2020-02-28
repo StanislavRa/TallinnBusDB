1840ac12bc4fe590f88a3e46a635766d2ad7d376
StanislavRa <s.ratsinski@hotmail.com>
2020-02-28 22:33:04 +0200

    Recreated BusController, cleaned up Main

--- src/main/java/controller/BusController.java
+++ src/main/java/controller/BusController.java
@@ -1,94 +1,98 @@
 package controller;
 
 import db.DatabaseHandler;
-import entity.Bus;
+
 import java.sql.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 public class BusController {
 
-	public int listAllBuses() {
-		DatabaseHandler databaseHandler = new DatabaseHandler();
-		Statement statement = databaseHandler.createStatement();
-		int id = -1;
-		System.out.println("List of all buses:");
-		try {
-			if (databaseHandler.getDbConnection() != null) {
-			ResultSet resultSetBuses = statement.executeQuery("SELECT * FROM buses");
-			while (resultSetBuses.next()) {
-				id = resultSetBuses.getInt("id");
-				String busNumber = resultSetBuses.getString("busNumber");
-				int driverId = resultSetBuses.getInt("driverId");
-				float fuel = resultSetBuses.getFloat("fuel");
-				Timestamp createdOn = resultSetBuses.getTimestamp("createdOn");
-				Date purchasedOn = resultSetBuses.getDate("purchasedOn");
+    public void listAllBuses() {
 
-				Bus newBus = new Bus(id, busNumber, driverId, fuel, createdOn, purchasedOn);
+        DatabaseHandler databaseHandler = new DatabaseHandler();
+        Statement statement = databaseHandler.createStatement();
 
-				System.out.println(newBus.toString());
-			}
-			statement.close();
-			databaseHandler.closeConnection();
-			}
-			return id;
-		} catch (SQLException ex) {
-			ex.printStackTrace();
-			return id;
-		}
-	}
+        System.out.println("List of all buses:");
 
-	public int findBusByNumber(String busNumber) {
-		DatabaseHandler databaseHandler = new DatabaseHandler();
-		Statement statement = databaseHandler.createStatement();
-		int id = -1;
-		try {
-			if (databaseHandler.getDbConnection() != null) {
-			ResultSet resultSetBuses = statement.executeQuery(
-					"SELECT * FROM buses where busNumber = '" + busNumber + "' limit 1");
-			boolean next = resultSetBuses.next();
-				if (next) {
-					id = resultSetBuses.getInt("id");
-					busNumber = resultSetBuses.getString("busNumber");
-					int driverId = resultSetBuses.getInt("driverId");
-					float fuel = resultSetBuses.getFloat("fuel");
-					Timestamp createdOn = resultSetBuses.getTimestamp("createdOn");
-					Date purchasedOn = resultSetBuses.getDate("purchasedOn");
-					Bus newBus = new Bus(id, busNumber, driverId, fuel, createdOn, purchasedOn);
+        try {
+            ResultSet resultSetBuses = statement.executeQuery("SELECT * FROM buses");
 
-					System.out.println("Bus with id: " + id);
-					System.out.println(newBus.toString());
-				}
-			}
-			return id;
-		} catch (Exception ex) {
-			ex.printStackTrace();
-			return id;
-		}
-	}
+            while (resultSetBuses.next()) {
 
-	public void create(String busNumber, int driverId, float fuel, String purchasedOn) {
-		DatabaseHandler databaseHandler = new DatabaseHandler();
-		Statement statement = databaseHandler.createStatement();
+                int id = resultSetBuses.getInt("id");
+                String busNumber = resultSetBuses.getString("busNumber");
+                int driverId = resultSetBuses.getInt("driverId");
+                float fuel = resultSetBuses.getFloat("fuel");
+                Timestamp createdOn = resultSetBuses.getTimestamp("createdOn");
+                Date purchasedOn = resultSetBuses.getDate("purchasedOn");
 
-		if (statement != null) {
-			try {
-				SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
-				java.util.Date purchasedOnStr = formatter.parse(purchasedOn);
-				java.sql.Date purchasedOnDB = new java.sql.Date(purchasedOnStr.getTime());
+                System.out.println(id + " " + busNumber + " " + driverId + " " +
+                        fuel + " " + createdOn + " " + purchasedOn);
+            }
+            statement.close();
+            databaseHandler.closeConnection();
 
-				statement.executeUpdate("INSERT INTO buses (busNumber, driverId, fuel, purchasedOn) VALUES" +
-						"('" + busNumber + "', '" + driverId + "', '" + fuel + "', '"+ purchasedOnDB + "')");
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
 
-				System.out.println("Inserted:");
-				findBusByNumber(busNumber);
+    public void findBusByNumber(String busNumber) {
 
-				statement.close();
-				databaseHandler.closeConnection();
+        DatabaseHandler databaseHandler = new DatabaseHandler();
+        Statement statement = databaseHandler.createStatement();
 
-			} catch (SQLException | ParseException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+        try {
+            ResultSet resultSetBuses = statement.executeQuery(
+                    "SELECT * FROM buses where busNumber = '" + busNumber + "' limit 1");
+
+            if (resultSetBuses.next()) {
+
+                int driverId = resultSetBuses.getInt("driverId");
+                float fuel = resultSetBuses.getFloat("fuel");
+                Timestamp createdOn = resultSetBuses.getTimestamp("createdOn");
+                Date purchasedOn = resultSetBuses.getDate("purchasedOn");
+
+                System.out.println("Here is found bus " + busNumber
+                        + ": driver id - " + driverId
+                        + ", fuel - " + fuel
+                        + " created on - " + createdOn
+                        + ", purchased on - " + purchasedOn);
+
+            } else System.out.println("Bus does not exist");
+
+            statement.close();
+            databaseHandler.closeConnection();
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void create(String busNumber, int driverId, float fuel, String purchasedOn) {
+
+        DatabaseHandler databaseHandler = new DatabaseHandler();
+        Statement statement = databaseHandler.createStatement();
+
+        try {
+            SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+            java.util.Date purchasedOnStr = formatter.parse(purchasedOn);
+            java.sql.Date purchasedOnDB = new java.sql.Date(purchasedOnStr.getTime());
+
+            statement.executeUpdate("INSERT INTO buses (busNumber, driverId, fuel, purchasedOn) VALUES" +
+                    "('" + busNumber + "', '" + driverId + "', '" + fuel + "', '" + purchasedOnDB + "')");
+
+			System.out.println("Inserted into the table bus " + busNumber
+					+ ": driver id - " + driverId
+					+ ", fuel - " + fuel
+					+ ", purchased on - " + purchasedOn);
+
+            statement.close();
+            databaseHandler.closeConnection();
+
+        } catch (SQLException | ParseException ex) {
+            ex.printStackTrace();
+        }
+    }
 }
--- src/main/java/domain/Domain.java
+++ src/main/java/domain/Domain.java
@@ -8,48 +8,5 @@ import controller.TimetableController;
 public class Domain {
 
 	public static void main(String[] args) {
-
-		DriverController driverController = new DriverController();
-		BusController busController = new BusController();
-    TimetableController timetableController = new TimetableController();
-
-		PassengersController passengersController = new PassengersController();
-    
-
-		driverController.listAllDrivers();
-		busController.listAllBuses();
-		passengersController.listAllPassengers();
-    driverController.findDriverByBusNumber("20B");
-		driverController.findDriverByBusNumber("20A");
-    timetableController.listAllTimetables();
-		timetableController.findTimetableForBusNumber("20A");
-// 		System.out.println("Let's add new Driver");
-// 		driverController.create("Ivanka Trump", "Tall building", "+1002555",25,174);
-// 		driverController.listAllDrivers();
-// 		System.out.println("Let's add a new Location");
-// 		timetableController.createTimetable(1, 1, "Monday", "07:25:00");
-
-
-
-// 		/*
-// 		@Oleks
-// 		 */
-// 		System.out.println("=========================================");
-// 		System.out.println("Let's add new Passenger");
-// 		passengersController.create("Som Son",
-// 								"somson@ukr.net",
-// 								"+10202555",
-// 								21,
-// 								1,
-// 								2,
-// 								3);
-// 		passengersController.listAllPassengers();
-
-// 		System.out.println("=========================================");
-// 		System.out.println("Passenger with id #3 information:");
-// 		passengersController.findByPassengerID(3);
-		/*
-		@Oleks
-		 */
 	}
 }
